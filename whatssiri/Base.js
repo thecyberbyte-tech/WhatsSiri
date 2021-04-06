2ca5bbb965eb693dc9051e347a50e93b733dcf2a
Erdem Bey <73111211+ErdemBey1@users.noreply.github.com>
2021-04-06 13:22:11 +0300

    Add files via upload

---
+++ whatssiri/Base.js
@@ -0,0 +1,20 @@
+/* BU BOT İSTEK ÜZERİNE YAPILMIŞ BİR BOTTUR
+OLASI DURUMLARDAN BİZ SORUMLULUK KABUL ETMİYORUZ SİRİ-DEV
+WhatsSiri SİRİ-ERDEM/BEY
+*/
+
+'use strict';
+
+class Base {
+    constructor(client) {
+        Object.defineProperty(this, 'client', { value: client });
+    }
+
+    _clone() {
+        return Object.assign(Object.create(this), this);
+    }
+    
+    _patch(data) { return data; }
+}
+
+module.exports = Base;
---
+++ whatssiri/index.js
@@ -0,0 +1,13 @@
+/* BU BOT İSTEK ÜZERİNE YAPILMIŞ BİR BOTTUR
+OLASI DURUMLARDAN BİZ SORUMLULUK KABUL ETMİYORUZ SİRİ-DEV
+WhatsSiri SİRİ-ERDEM/BEY
+*/
+
+module.exports = {
+    Base: require('./Base'),
+    Message: require('./Message'),
+    StringSession: require('./StringSession'),
+    ReplyMessage: require('./ReplyMessage'),
+    Image: require('./Image'),
+    Video: require('./Video')
+};
