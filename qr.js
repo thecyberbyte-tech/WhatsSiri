19638842a97ff949643c24795adc9831ee164c4d
Erdem Bey <73111211+ErdemBey1@users.noreply.github.com>
2021-04-06 15:18:29 +0300

    Add files via upload

--- qr.js
+++ qr.js
@@ -1,38 +1,35 @@
-/* Copyright (C) 2020 Yusuf Usta.
-
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-
-WhatsAsena - Yusuf Usta
+/* BU BOT İSTEK ÜZERİNE YAPILMIŞ BİR BOTTUR
+OLASI DURUMLARDAN BİZ SORUMLULUK KABUL ETMİYORUZ SİRİ-DEV
+WhatsSiri SİRİ-ERDEM/BEY
 */
 
 const chalk = require('chalk');
 const {WAConnection} = require('@adiwajshing/baileys');
-const {StringSession} = require('./whatsasena/');
+const {StringSession} = require('./whatssiri/');
 const fs = require('fs');
 
-async function whatsAsena () {
+async function whatsSiri () {
     const conn = new WAConnection();
     const Session = new StringSession();  
     conn.logger.level = 'warn';
-    conn.regenerateQRIntervalMs = 30000;
+    conn.regenerateQRIntervalMs = 50000;
     
     conn.on('connecting', async () => {
-        console.log(`${chalk.green.bold('Whats')}${chalk.blue.bold('Asena')}
-${chalk.white.italic('AsenaString Kodu Alıcı')}
+        console.log(`${chalk.green.bold('Whats')}${chalk.blue.bold('Siri')}
+${chalk.white.italic('SiriString Kodu Alıcı')}
 
-${chalk.blue.italic('ℹ️  Connecting to Whatsapp... Please wait.')}`);
+${chalk.blue.italic('ℹ️  Connecting to Whatsapp... Please Wait.')}`);
     });
     
 
     conn.on('open', () => {
         var st = Session.createStringSession(conn.base64EncodedAuthInfo());
         console.log(
-            chalk.green.bold('Asena String Kodunuz: '), Session.createStringSession(conn.base64EncodedAuthInfo())
+            chalk.green.bold('Siri String Kodunuz: '), Session.createStringSession(conn.base64EncodedAuthInfo())
         );
         
         if (!fs.existsSync('config.env')) {
-            fs.writeFileSync('config.env', `ASENA_SESSION="${st}"`);
+            fs.writeFileSync('config.env', `SIRI_SESSION="${st}"`);
         }
 
         console.log(
@@ -44,4 +41,4 @@ ${chalk.blue.italic('ℹ️  Connecting to Whatsapp... Please wait.')}`);
     await conn.connect();
 }
 
-whatsAsena()
+whatsSiri()
