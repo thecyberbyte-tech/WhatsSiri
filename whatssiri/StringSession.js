de42fe2332b57e361777f25bcf489b2a022df62a
Erdem Bey <73111211+ErdemBey1@users.noreply.github.com>
2021-04-06 13:20:56 +0300

    Add files via upload

---
+++ whatssiri/StringSession.js
@@ -0,0 +1,32 @@
+/* BU BOT İSTEK ÜZERİNE YAPILMIŞ BİR BOTTUR
+OLASI DURUMLARDAN BİZ SORUMLULUK KABUL ETMİYORUZ SİRİ-DEV
+WhatsSiri SİRİ-ERDEM/BEY
+*/
+
+const fs = require('fs');
+
+class StringSession {
+    constructor() {
+    }
+
+    deCrypt(string = undefined) {
+        if ('SIRI_SESSION' in process.env && string === undefined) {
+            string = process.env.STRING_SESSION;
+        } else if (string !== undefined) {
+            if (fs.existsSync(string)) {
+                string = fs.readFileSync(string, {encoding:'utf8', flag:'r'});
+            }
+        }
+        
+        var split = string.split(';;;');
+        if (split.length >= 2) {
+            return JSON.parse(Buffer.from(split[split.length - 1], 'base64').toString('utf-8'));
+        }
+    }
+
+    createStringSession(dict) {
+        return 'SIRI;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
+    }
+}
+
+module.exports = StringSession;
