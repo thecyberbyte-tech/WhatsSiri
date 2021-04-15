c618e4812f80bf8b2bac5677e371ee2b1650e1b9
Erdem Bey <73111211+ErdemBey1@users.noreply.github.com>
2021-04-15 23:59:15 +0300

    v1.0 HAZIR - Kodlayıcı ERDEM BEY

--- plugins/sql/notes.js
+++ plugins/sql/notes.js
@@ -1,19 +1,11 @@
-/*
-# Copyright (C) 2020 MuhammedKpln.
-#
-# WhatsSiri is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# WhatsSiri is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-#
+/* BU BOT İSTEK ÜZERİNE YAPILMIŞ BİR BOTTUR
+OLASI DURUMLARDAN BİZ SORUMLULUK KABUL ETMEZ
+Telegram / @orgutsahibi
+GitHub / ErdemBey1
+
+WhatsSiri SİRİ-ERDEM/BEY
+
+OWNER|ErdemBey
 */
 
 const config = require('../../config');
