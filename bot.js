c8d780e8ea3e4f3041f440b7191e5b810e4147dd
Erdem Bey <73111211+ErdemBey1@users.noreply.github.com>
2021-04-16 21:07:25 +0300

    EDİT

--- bot.js
+++ bot.js
@@ -1,7 +1,13 @@
 /* BU BOT İSTEK ÜZERİNE YAPILMIŞ BİR BOTTUR
 OLASI DURUMLARDAN BİZ SORUMLULUK KABUL ETMEZ
 
-Tg: SiriDev
+Telegram / @orgutsahibi
+
+GitHub / ErdemBey1
+
+WhatsSiri SİRİ-ERDEM/BEY
+
+OWNER|ErdemBey
 */
 
 
