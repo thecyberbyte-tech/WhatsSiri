5f1eeeb60a361005831126e2badd8dbdc031861e
Erdem Bey <73111211+ErdemBey1@users.noreply.github.com>
2021-04-25 17:37:19 +0300

    as geldi

---
+++ plugins/as.js
@@ -0,0 +1,19 @@
+const Siri = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+
+Siri.addCommand({pattern: 'sa', fromMe: true}, (async (message, match) => {
+
+    await message.sendMessage('*oooooo*');
+    await message.sendMessage('*Kimleri Görüyorum*');
+  
+    await new Promise(r => setTimeout(r, 1000));
+   
+    await message.sendMessage('*Aleyküm Selam*');
+
+}));
+
+Siri.addCommand({pattern: 'infoas', fromMe: true}, (async (message, match) => {
+
+    await message.sendMessage("*Codded by Erdem Bey*\n*WhatsApptada Selamlara Karşılık Verelim dimi.*\n💻Usage: *.as*")
+
+}));
