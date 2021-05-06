6beaa542b3e19351efba72cdec9724d01c554b09
Erdem Bey <73111211+ErdemBey1@users.noreply.github.com>
2021-05-06 06:58:46 +0300

    👌

--- events.js
+++ events.js
@@ -1,6 +1,6 @@
 /* BU BOT İSTEK ÜZERİNE YAPILMIŞ BİR BOTTUR
 OLASI DURUMLARDAN BİZ SORUMLULUK KABUL ETMEZ
-Telegram / @orgutsahibi
+Telegram / @erdembey1
 GitHub / ErdemBey1
 
 WhatsSiri SİRİ-ERDEM/BEY
