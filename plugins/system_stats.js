b592c76dec1c5e38512b0d880222918b142a46e9
Sam Pandey <69663175+sam-pandey@users.noreply.github.com>
2021-05-19 07:33:06 +0530

    Update system_stats.js

--- plugins/system_stats.js
+++ plugins/system_stats.js
@@ -29,9 +29,9 @@ if (Config.WORKTYPE == 'private') {
         if (Config.ALIVEMSG == 'default') {
             await message.client.sendMessage(
             message.jid, 
-            fs.readFileSync("/root/WhatsSiri/Media/photo.jpg"),
+            fs.readFileSync("/root/WhatsSiri/Media/gif/photo.jpg"),
             MessageType.image, 
-            { mimetype: Mimetype.image, caption: "*●▬▬▬ Siri A Userbot  ▬▬▬●*\n*Hii,I'm Siri.*\n```I do many cool tasks for my owner 24 hours and 7 days in week```\n*Siri works for it's Owner*\n```All things are working Perfectly```\n*Grade:* ```Developer```\n*Branch:* ```Master```\n*My Favorite Work:*```Coding```\n\n*Powered by Mr Sam*" }
+            { mimetype: Mimetype.image, caption: "*●▬▬▬ Siri A Userbot  ▬▬▬●*\n*Hii,I'm Siri.*\n```I do many cool tasks for my owner 24 hours and 7 days in week```\n*Siri works for it's Owner*\n```All things are working Perfectly```\n*Grade:* ```Developer```\n*Branch:* ```Master```\n*My Favorite Work:*```Doing Service```\n\n*Developed by Mr Sam*" }
         )
     }
         else {
