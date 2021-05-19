89d3723ade6a5d442f8b61d7e6d4b14d4abe9cb8
Sam Pandey <69663175+sam-pandey@users.noreply.github.com>
2021-05-19 07:25:31 +0530

    Update system_stats.js

--- plugins/system_stats.js
+++ plugins/system_stats.js
@@ -14,6 +14,10 @@ const {spawnSync} = require('child_process');
 const Config = require('../config');
 const chalk = require('chalk');
 
+const fs = require('fs');
+
+
+
 const Language = require('../language');
 const Lang = Language.getString('system_stats');
 
@@ -23,8 +27,13 @@ if (Config.WORKTYPE == 'private') {
     Siri.addCommand({pattern: 'alive', fromMe: true, desc: Lang.ALIVE_DESC}, (async (message, match) => {
 
         if (Config.ALIVEMSG == 'default') {
-            await message.client.sendMessage(message.jid,'```Siri Her Yerde Siri Şuan Sahibin Hizmetinde!```\n\n*Version:* ```'+Config.VERSION+'```\n*Branch:* ```'+Config.BRANCH+'```\n*Telegram Group:* https://t.me/SiriSupport\n*Telegram Channel:* https://t.me/WhatsSiri' , MessageType.text);
-        }
+            await message.client.sendMessage(
+            message.jid, 
+            fs.readFileSync("/root/WhatsSiri/Media/photo.jpg"),
+            MessageType.image, 
+            { mimetype: Mimetype.image, caption: "*●▬▬▬ Siri A Userbot  ▬▬▬●*\n*Hii,I'm Siri.*\n```I do many cool tasks for my owner 24 hours and 7 days in week```\n*Siri works for it's Owner*\n```All things are working Perfectly```\n*Grade:* ```Developer```\n*Branch:* ```Master```\n*My Favorite Work:*```Coding```\n\n*Powered by Mr Sam*" }
+        )
+    }
         else {
             await message.client.sendMessage(message.jid,Config.ALIVEMSG + '\n*Powered by WhatsSiri*', MessageType.text);
         }
